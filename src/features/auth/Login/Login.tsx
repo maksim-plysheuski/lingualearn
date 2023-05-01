fdf22c30b2b109ae89fab20a20467100b626937f
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2023-05-01 21:35:53 +0300

    validation login (password, email)

--- src/features/auth/Login/Login.tsx
+++ src/features/auth/Login/Login.tsx
@@ -17,9 +17,15 @@ export const Login = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <FormControl>
-        <TextField {...register("email")} />
-
-        <TextField {...register("password")} />
+        <TextField {...register("email", {
+          required: "Field is required",
+          pattern: {value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, message: "Invalid Email"},
+          maxLength: {value: 20, message: "Length must be less then 20 symbols"}
+        })} />
+        {errors.email && <span>{errors.email.message}</span>}
+        <TextField {...register("password", {required: "Field is required"}
+        )} />
+        {errors.password && <span>{errors.password.message}</span>}
         <input type="submit" />
       </FormControl>
     </form>
