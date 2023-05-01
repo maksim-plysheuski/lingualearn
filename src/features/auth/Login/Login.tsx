0beee50ee43b95024d72396fc1cbce6dc7b6402b
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2023-05-02 00:53:31 +0300

    login refactoring

--- src/features/auth/Login/Login.tsx
+++ src/features/auth/Login/Login.tsx
@@ -1,5 +1,16 @@
 import { useForm, SubmitHandler } from "react-hook-form";
-import { FormControl, TextField } from "@mui/material";
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  IconButton,
+  InputAdornment,
+  TextField
+} from "@mui/material";
+import { useState } from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
 
 type InputsType = {
   email: string,
@@ -7,27 +18,58 @@ type InputsType = {
 };
 
 export const Login = () => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const showPasswordHandler = () => setShowPassword(!showPassword);
+
   const { register, handleSubmit, watch, formState: { errors } } = useForm<InputsType>();
   const onSubmit: SubmitHandler<InputsType> = (data: InputsType) => {
     console.log(data);
   };
-  console.log(errors);
 
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <FormControl>
-        <TextField {...register("email", {
-          required: "Field is required",
-          pattern: {value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, message: "Invalid Email"},
-          maxLength: {value: 20, message: "Length must be less then 20 symbols"}
-        })} />
-        {errors.email && <span>{errors.email.message}</span>}
-        <TextField {...register("password", {required: "Field is required"}
-        )} />
-        {errors.password && <span>{errors.password.message}</span>}
-        <input type="submit" />
-      </FormControl>
-    </form>
+    <div>
+      <h1>Sign In</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl>
+          <TextField type={"text"}
+                     label={"Email"}
+                     variant="standard"
+                     error={!!errors.email}
+                     helperText={errors.email?.message}
+                     {...register("email", {
+                       required: "Field is required",
+                       pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, message: "Invalid Email" },
+                       maxLength: { value: 30, message: "Length must be less then 20 symbols" }
+                     })} />
+          <TextField type={showPassword ? "text" : "password"}
+                     label={"Password"}
+                     variant="standard"
+                     error={!!errors.password}
+                     helperText={errors.password?.message}
+                     InputProps={{
+                       endAdornment: (
+                         <InputAdornment position="end">
+                           <IconButton
+                             aria-label="toggle password visibility"
+                             onClick={showPasswordHandler}
+                           >
+                             {showPassword ? <Visibility /> : <VisibilityOff />}
+                           </IconButton>
+                         </InputAdornment>
+                       )
+                     }}
+                     {...register("password", { required: "Field is required" })}
+          />
+          <FormControlLabel control={<Checkbox />} label="Remember Me" />
+          <Button
+            type={"submit"}
+            variant={"contained"}
+            color={"primary"}>
+            Sign In
+          </Button>
+        </FormControl>
+      </form>
+    </div>
   );
 };
