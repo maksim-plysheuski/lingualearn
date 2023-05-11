87e8e6dc4a2c3ff9947f982f0bb31ed61bf73c30
leskevich <leskevichtema@gmail.com>
2023-05-11 21:57:36 +0300

    fix: CreateNewPassword

--- src/features/auth/createNewPassword/CreateNewPassword.tsx
+++ src/features/auth/createNewPassword/CreateNewPassword.tsx
@@ -1,13 +1,20 @@
 import React, { FC } from 'react'
+import s from './style.module.scss'
 
 type Props = {
-  value:string
+  value: string
 }
 
-export const CreateNewPassword:FC<Props> = () => {
-  return (
-    <div>
+export const CreateNewPassword: FC<Props> = () => {
 
+  return (
+    <div className={s.page}>
+      <div className={s.container}>
+        <form className={s.form}>
+
+
+        </form>
+      </div>
     </div>
   )
 }
