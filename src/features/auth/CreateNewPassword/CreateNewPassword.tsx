64b4cedbaf2a0c92e2cfd5454d66a42deacd29b9
leskevich <leskevichtema@gmail.com>
2023-05-12 10:10:40 +0300

    fix: CreateNewPassword

--- src/features/auth/CreateNewPassword/CreateNewPassword.tsx
+++ src/features/auth/CreateNewPassword/CreateNewPassword.tsx
@@ -1,18 +1,45 @@
-import React, { FC } from 'react'
+import React from 'react'
 import s from './style.module.scss'
+import * as yup from 'yup'
+import { passwordSchema } from 'common/utils/schemas'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { InputPassword } from 'common/components/Inputs/InputPassword'
+import { useParams } from 'react-router-dom'
+import { useAppDispatch } from 'app/hooks'
+import { authThunks } from 'features/auth/auth.slice'
 
-type Props = {
-  value: string
-}
 
-export const CreateNewPassword: FC<Props> = () => {
+type Type = yup.InferType<typeof passwordSchema>
+
+export const CreateNewPassword = () => {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid }
+  } = useForm<Type>({
+    mode: 'onBlur',
+    resolver: yupResolver(passwordSchema)
+  })
+
+  const dispatch = useAppDispatch()
+  const { token } = useParams<{ token: string }>()
+
+  const onSubmit: SubmitHandler<Type> = ({ password }) => {
+    if (token) dispatch(authThunks.newPassword({ password, resetPasswordToken: token }))
+  }
 
   return (
     <div className={s.page}>
       <div className={s.container}>
-        <form className={s.form}>
-
-
+        <span>Create new password</span>
+        <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+          <InputPassword className={s.inputPassword}
+                         errorMessage={errors.password?.message}
+                         register={register('password')} />
+          <p className={s.helperText}>Create new password and we will send you further instructions to email</p>
+          <button disabled={!isValid} className={s.button}>Create new password</button>
         </form>
       </div>
     </div>
