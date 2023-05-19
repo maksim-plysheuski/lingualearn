2c778e664bbd3b5d59e2873667a73ecb4cfa28be
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2023-05-19 15:07:47 +0300

    fix: profilePage navLink path, authSlice

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -3,21 +3,26 @@ import logo from 'common/components/Header/projectLogo.svg'
 import { UniversalButton } from 'common/components/Button/UniversalButton'
 import { useNavigate } from 'react-router-dom'
 import React from 'react'
+import { paths } from 'common/router/path'
 
 export const Header = () => {
   const navigate = useNavigate()
+  const singUpCallback = () => navigate(paths.REGISTER)
 
-  const singUpCallback = () => navigate('/registration')
 
   return (
-      <header className={s.header}>
-        <div className={s.headerContainer}>
-          <a href='/' className={s.logoBlock}>
-            <img src={logo} alt='' />
-            <span>Lingualearn</span>
-          </a>
-          <UniversalButton title='Sign Up' onClickCallback={singUpCallback} />
-        </div>
-      </header>
+    <header className={s.header}>
+      <div className={s.headerContainer}>
+        <a href='/' className={s.logoBlock}>
+          <img src={logo} alt='' />
+          <span>Lingualearn</span>
+        </a>
+        <UniversalButton title='Sign Up'
+                         onClickCallback={singUpCallback}
+                         rounded={true}
+                         textColor={'white'}
+                         width={'110'} />
+      </div>
+    </header>
   )
 }
