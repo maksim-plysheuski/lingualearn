ceb809d5be4992a9fdc52767f667233c221f9466
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2023-05-20 21:19:16 +0300

    fix: paginator error / styles

--- src/features/pack/packsList/packsTable/packsTableBody/PacksTableBody.tsx
+++ src/features/pack/packsList/packsTable/packsTableBody/PacksTableBody.tsx
@@ -7,20 +7,25 @@ export const PacksTableBody = () => {
   const tableCellStyle = {
     wordWrap: 'break-word',
     minWidth: '150px',
-    maxWidth: '200px',
+    maxWidth: '200px'
   }
 
   return (
     <TableBody>
       {packs.cardPacks?.map((card) => (
         <TableRow key={card._id}>
-          <TableCell sx={{ ...tableCellStyle, paddingLeft: '40px', cursor: 'pointer', ':hover': { backgroundColor: 'rgb(245, 245, 245)' }}}>
+          <TableCell sx={{
+            ...tableCellStyle,
+            paddingLeft: '40px',
+            cursor: 'pointer',
+            ':hover': { backgroundColor: 'rgb(245, 245, 245)' }
+          }}>
             {card.name}
           </TableCell>
-          <TableCell sx={{tableCellStyle}}>{card.cardsCount}</TableCell>
-          <TableCell sx={{tableCellStyle}}>{card.updated}</TableCell>
-          <TableCell sx={{tableCellStyle}}>{card.user_name}</TableCell>
-          <TableCell sx={{tableCellStyle}}>"need to fix"</TableCell>
+          <TableCell sx={{ tableCellStyle }}>{card.cardsCount}</TableCell>
+          <TableCell sx={{ tableCellStyle }}>{card.updated}</TableCell>
+          <TableCell sx={{ tableCellStyle }}>{card.user_name}</TableCell>
+          <TableCell sx={{ tableCellStyle }}>"need to fix"</TableCell>
         </TableRow>
       ))}
     </TableBody>
--- src/features/pack/packsList/paginator/Paginator.tsx
+++ src/features/pack/packsList/paginator/Paginator.tsx
@@ -1,7 +1,8 @@
-import { Box, FormControl, Pagination, Select, SelectChangeEvent } from '@mui/material'
+import { Pagination, Select, SelectChangeEvent } from '@mui/material'
 import MenuItem from '@mui/material/MenuItem'
 import { packsThunks } from 'features/pack/packs.slice'
 import { useAppDispatch, useAppSelector } from 'common/hooks'
+import s from './style.module.scss'
 
 
 export const Paginator = () => {
@@ -9,7 +10,7 @@ export const Paginator = () => {
   const currentPage = useAppSelector(state => state.packs.packs.page)
   const packsTotalCount = useAppSelector(state => state.packs.packs.cardPacksTotalCount)
   const rowsInTable = useAppSelector(state => state.packs.packs.pageCount)
-  const pagesTotalCount = Math.ceil(packsTotalCount / rowsInTable)
+  const pagesTotalCount = Math.ceil(packsTotalCount / rowsInTable) || 0
 
 
   const handleRowsChange = (event: SelectChangeEvent) => {
@@ -21,27 +22,20 @@ export const Paginator = () => {
   }
 
   return (
-    <Box component={'div'} sx={{ margin: '36px 0 72px 0', width: 1008, display: 'flex', alignItems: 'center' }}>
-      <Pagination count={pagesTotalCount}
-                  shape='rounded'
-                  color='primary'
-                  onClick={handlePagesChange} />
-
-      <FormControl sx={{ minWidth: 50, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-        <Box component={'span'} sx={{ marginLeft: '20px' }}>Show</Box>
-        <Select
-          sx={{ height: '34px', margin: '0 7px 0 7px' }}
-          defaultValue={'8'}
-          value={rowsInTable ? String(rowsInTable) : '8'}
-          onChange={handleRowsChange}
-        >
-          <MenuItem value={'8'}>8</MenuItem>
-          <MenuItem value={'15'}>15</MenuItem>
-          <MenuItem value={'25'}>25</MenuItem>
-          <MenuItem value={'50'}>50</MenuItem>
-        </Select>
-        <Box component={'span'}>Cards per page</Box>
-      </FormControl>
-    </Box>
+    <div className={s.container}>
+      <Pagination shape='rounded' color='primary' count={pagesTotalCount} onClick={handlePagesChange} />
+      <span>Show</span>
+      <Select
+        sx={{ height: '34px', margin: '0 7px 0 7px' }}
+        value={rowsInTable ? String(rowsInTable) : '8'}
+        onChange={handleRowsChange}
+      >
+        <MenuItem value={'8'}>8</MenuItem>
+        <MenuItem value={'15'}>15</MenuItem>
+        <MenuItem value={'25'}>25</MenuItem>
+        <MenuItem value={'50'}>50</MenuItem>
+      </Select>
+      <span>Cards per page</span>
+    </div>
   )
 }
