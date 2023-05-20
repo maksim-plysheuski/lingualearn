767ba7187177b38ca43bea5fb82774da800be166
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2023-05-20 19:49:51 +0300

    fix: packsTable key map

--- src/features/pack/packsList/packsTable/packsTableBody/PacksTableBody.tsx
+++ src/features/pack/packsList/packsTable/packsTableBody/PacksTableBody.tsx
@@ -4,20 +4,23 @@ import { useAppSelector } from 'common/hooks'
 export const PacksTableBody = () => {
   const packs = useAppSelector(state => state.packs.packs)
 
+  const tableCellStyle = {
+    wordWrap: 'break-word',
+    minWidth: '150px',
+    maxWidth: '200px',
+  }
+
   return (
     <TableBody>
-      {packs.cardPacks?.map((card, index) => (
-        <TableRow
-          key={index}
-          sx={{ '&:last-child td, &:last-child th': { border: 0 }, height: 48 }}
-        >
-          <TableCell component='th' scope='row'>
+      {packs.cardPacks?.map((card) => (
+        <TableRow key={card._id}>
+          <TableCell sx={{ ...tableCellStyle, paddingLeft: '40px', cursor: 'pointer', ':hover': { backgroundColor: 'rgb(245, 245, 245)' }}}>
             {card.name}
           </TableCell>
-          <TableCell align='right'>{card.cardsCount}</TableCell>
-          <TableCell align='right'>{card.updated}</TableCell>
-          <TableCell align='right'>{card.user_name}</TableCell>
-          <TableCell align='right'>"need to fix"</TableCell>
+          <TableCell sx={{tableCellStyle}}>{card.cardsCount}</TableCell>
+          <TableCell sx={{tableCellStyle}}>{card.updated}</TableCell>
+          <TableCell sx={{tableCellStyle}}>{card.user_name}</TableCell>
+          <TableCell sx={{tableCellStyle}}>"need to fix"</TableCell>
         </TableRow>
       ))}
     </TableBody>
