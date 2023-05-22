d1ef985289830e8f15e249bbe27eb14eca9e3924
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2023-05-22 19:33:59 +0300

    feat: add action buttons for packs table

--- src/features/pack/packsList/packsTable/packsTableBody/PacksTableBody.tsx
+++ src/features/pack/packsList/packsTable/packsTableBody/PacksTableBody.tsx
@@ -1,8 +1,13 @@
 import { TableBody, TableCell, TableRow } from '@mui/material'
 import { useAppSelector } from 'common/hooks'
+import SchoolIcon from '@mui/icons-material/School'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton'
 
 export const PacksTableBody = () => {
   const packs = useAppSelector(state => state.packs.packs)
+  const userId = useAppSelector(state => state.packs.packParams.user_id)
 
   const tableCellStyle = {
     wordWrap: 'break-word',
@@ -25,7 +30,17 @@ export const PacksTableBody = () => {
           <TableCell sx={{ tableCellStyle }}>{card.cardsCount}</TableCell>
           <TableCell sx={{ tableCellStyle }}>{card.updated.slice(0, 10).replaceAll('-', '.')}</TableCell>
           <TableCell sx={{ tableCellStyle }}>{card.user_name}</TableCell>
-          <TableCell sx={{ tableCellStyle }}>"need to fix"</TableCell>
+          <TableCell sx={{ tableCellStyle }}>
+            <IconButton>
+              <SchoolIcon></SchoolIcon>
+            </IconButton>
+            <IconButton disabled={!userId}>
+              <EditIcon></EditIcon>
+            </IconButton>
+            <IconButton disabled={!userId}>
+              <DeleteIcon></DeleteIcon>
+            </IconButton>
+          </TableCell>
         </TableRow>
       ))}
     </TableBody>
