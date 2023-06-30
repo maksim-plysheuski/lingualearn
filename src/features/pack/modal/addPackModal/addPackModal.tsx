c6ddc9206952719b992cb6d32ffadf4df42a0384
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2023-06-30 22:44:18 +0300

    feet: add cover image to new pack

--- src/features/pack/modal/addPackModal/addPackModal.tsx
+++ src/features/pack/modal/addPackModal/addPackModal.tsx
@@ -2,9 +2,12 @@ import { BaseModal } from 'common/components/baseModal/BaseModal'
 import { InputCastom } from 'common/components/baseModal/inputCastom/InputCastom'
 import { UniversalButton } from 'common/components/universalButton/UniversalButton'
 import { useAppDispatch } from 'common/hooks'
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { packsThunks } from '../../packs.slice'
-
+import defAva from 'features/profile/components/imegs/defAva.png'
+import { convertFileToBase64 } from 'common/utils'
+import { Button } from '@mui/material'
+import { toast } from 'react-toastify'
 
 
 export const AddPackModal = () => {
@@ -13,13 +16,32 @@ export const AddPackModal = () => {
   const [valuePack, setValueCard] = useState<string>('')
   const [privatePack, setPrivatePack] = useState<boolean>(false)
   const [disable, setDisable] = useState<boolean>(false)
+  const [deckCover, setDeckCover] = useState<string>('')
 
   const addPackHandler = async () => {
     setDisable(true)
-    await dispatch(packsThunks.createPack({ name: valuePack, private: privatePack }))
-    setValueCard('')
-    setOpen(false)
-    setDisable(false)
+    const newPack = { name: valuePack, private: privatePack, deckCover: deckCover }
+    await dispatch(packsThunks.createPack(newPack)).unwrap()
+      .then(() => toast.success('Cover Added'))
+      .catch(err => toast.error('Error'))
+      .finally(() => {
+        setValueCard('')
+        setOpen(false)
+        setDisable(false)
+      })
+  }
+
+  const uploadHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length) {
+      const file = e.target.files[0]
+      if (file.size < 4000000) {
+        convertFileToBase64(file, async (file64: string) => {
+          setDeckCover(file64)
+        })
+      } else {
+        console.error('Error: ', 'Файл слишком большого размера')
+      }
+    }
   }
 
   return (
@@ -32,13 +54,19 @@ export const AddPackModal = () => {
                disable={disable}
     >
       <>
+        <div>
+          <img src={deckCover ? deckCover : defAva} alt='pack image' style={{ height: '90px', width: '90px' }} />
+          <label>
+            <Button variant='contained' component='span'>Change cover</Button>
+            <input type='file' style={{ display: 'none' }} onChange={uploadHandler} />
+          </label>
+        </div>
         <InputCastom label={'Name Pack'} value={valuePack} setValue={setValueCard} />
         <div>
           <input type='checkbox' checked={privatePack} onClick={() => setPrivatePack(state => !state)} />
           <span>Private pack</span>
         </div>
       </>
-
     </BaseModal>
   )
 }
