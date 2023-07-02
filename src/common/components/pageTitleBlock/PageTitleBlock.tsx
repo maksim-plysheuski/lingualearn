4fd86e9867f0b3882bf4924b5e650573d0e3e3b0
leskevich <leskevichtema@gmail.com>
2023-07-02 12:47:38 +0300

    fix: PageTitleBlock.tsx select

--- src/common/components/pageTitleBlock/PageTitleBlock.tsx
+++ src/common/components/pageTitleBlock/PageTitleBlock.tsx
@@ -2,10 +2,11 @@ import s from './style.module.scss'
 import * as React from 'react'
 import { useAppSelector } from 'common/hooks'
 import { AddPackModal } from 'features/pack/modal/addPackModal/addPackModal'
+import { selectPackUserId } from 'features/cards/selectors'
 
 
 export const PageTitleBlock = () => {
-  const packOwnerId = useAppSelector(state => state.cards.cards.packUserId)
+  const packOwnerId = useAppSelector(selectPackUserId)
   const userId = useAppSelector(state => state.profile.profile?._id)
 
   return (
