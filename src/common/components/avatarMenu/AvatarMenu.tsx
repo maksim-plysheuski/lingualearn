c0ced069af2674961af65a4400b5a06607b27498
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2023-07-07 19:53:24 +0300

    refactor: avatarMenu

--- src/common/components/avatarMenu/AvatarMenu.tsx
+++ src/common/components/avatarMenu/AvatarMenu.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react'
-import Box from '@mui/material/Box'
 import Avatar from '@mui/material/Avatar'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import Divider from '@mui/material/Divider'
 import IconButton from '@mui/material/IconButton'
-import Typography from '@mui/material/Typography'
 import Tooltip from '@mui/material/Tooltip'
 import Logout from '@mui/icons-material/Logout'
 import { useAppDispatch, useAppSelector } from 'common/hooks'
 import { useNavigate } from 'react-router-dom'
 import { paths } from 'common/router/path'
 import { authThunks } from 'features/auth/auth.slice'
+import s from './style.module.scss'
+import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined'
+import { menuStyle, paperStyle } from 'common/components/avatarMenu/style'
 
 
 export const AvatarMenu = () => {
@@ -23,111 +24,46 @@ export const AvatarMenu = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
   const open = Boolean(anchorEl)
 
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget)
-  }
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
+  const handleClickMenu = (event: React.MouseEvent<HTMLElement>) => setAnchorEl(event.currentTarget)
+  const handleCloseMenu = () => setAnchorEl(null)
+  const openProfilePage = () => navigate(paths.PROFILE)
+  const logout = () => dispatch(authThunks.logout())
 
-  const handleProfileButton = () => {
-    navigate(paths.PROFILE)
-  }
-
-  const handleLogoutButton = () => {
-    dispatch(authThunks.logout())
-  }
-
-  const style = {
-    '& .MuiPaper-root': {
-      bgcolor: '#171717',
-      border: '1px solid #333333',
-      marginTop: 1,
-      minWidth: 180,
-      color: '#fff',
-      '& .MuiMenu-list': {
-        padding: '4px 0'
-      },
-      '& .MuiMenuItem-root': {
-        '&:hover': {
-          bgcolor: '#333333'
-        },
-        '& .MuiSvgIcon-root': {
-          fontSize: 18,
-          color: '#fff',
-          marginRight: 1
-        }
-      }
-    }
-  }
 
   return (
-    <React.Fragment>
-      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'right' }}>
-        <Typography sx={{ minWidth: 100, color: 'white' }}>
-          {userName}
-        </Typography>
-        <Tooltip title='Account settings'>
-          <IconButton
-            onClick={handleClick}
-            size='small'
-            sx={{ ml: 2 }}
-            aria-controls={open ? 'account-menu' : undefined}
-            aria-haspopup='true'
-            aria-expanded={open ? 'true' : undefined}
-          >
-            <Avatar sx={{ width: 32, height: 32 }} src={profile?.avatar} />
+    <>
+      <div className={s.headerAvaContainer}>
+        <span>{userName}</span>
+        <Tooltip title='Account menu'>
+          <IconButton onClick={handleClickMenu}>
+            <Avatar src={profile?.avatar} />
           </IconButton>
         </Tooltip>
-      </Box>
-      <Menu
-        sx={style}
-        anchorEl={anchorEl}
-        id='account-menu'
-        open={open}
-        onClose={handleClose}
-        onClick={handleClose}
-        PaperProps={{
-          elevation: 0,
-          sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-            mt: 1.5,
-            '& .MuiAvatar-root': {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1
-            },
-            '&:before': {
-              content: '""',
-              display: 'block',
-              position: 'absolute',
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
-              zIndex: 0
-            }
-          }
-        }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-      >
-        <MenuItem onClick={handleProfileButton}>
-          <Avatar src={profile?.avatar} /> {userName}
-
+      </div>
+      <Menu sx={menuStyle} anchorEl={anchorEl} id='account-menu' open={open}
+            onClose={handleCloseMenu} onClick={handleCloseMenu} PaperProps={paperStyle}
+            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}>
+        <div className={s.userInfoContainer}>
+          <Avatar src={profile?.avatar} />
+          <div className={s.userInfo}>
+            <h3>{userName}</h3>
+            <span>{profile?.email}</span>
+          </div>
+        </div>
+        <Divider />
+        <MenuItem onClick={openProfilePage}>
+          <ListItemIcon>
+            <PermIdentityOutlinedIcon />
+          </ListItemIcon>My Profile
         </MenuItem>
         <Divider />
-        <MenuItem onClick={handleLogoutButton}>
+        <MenuItem onClick={logout}>
           <ListItemIcon>
-            <Logout fontSize='small' />
-          </ListItemIcon>
-          Sign Out
+            <Logout />
+          </ListItemIcon>Sign Out
         </MenuItem>
       </Menu>
-    </React.Fragment>
+    </>
   )
 }
