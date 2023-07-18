f2b4751e95fd98a0ccfe1207997351049cecc295
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2023-07-18 14:09:13 +0300

    refactor: title packs page

--- src/features/pack/components/packsList/titleBlock/TitleBlock.tsx
+++ src/features/pack/components/packsList/titleBlock/TitleBlock.tsx
@@ -5,7 +5,7 @@ import {AddPackModal} from 'features/pack/modal/addPackModal/addPackModal'
 export const TitleBlock = () => {
   return (
     <div className={s.addPackBlock}>
-      <h2>Packs</h2>
+      <h2>Packs List</h2>
       <AddPackModal />
     </div>
   )
