a14ac7007b20642b03b51a4d4178df11a680714c
leskevich <leskevichtema@gmail.com>
2023-07-27 11:48:15 +0300

    fix: skeletonPreloaders index.ts

---
+++ src/common/components/skeletonPreloaders/index.ts
@@ -0,0 +1,6 @@
+export { PaginatorSkeleton } from 'common/components/skeletonPreloaders/PaginatorSkeleton'
+export { SearchBarSkeleton } from 'common/components/skeletonPreloaders/SearchBarSkeleton'
+export { SelectorSkeleton } from 'common/components/skeletonPreloaders/SelectorSceleton'
+export { TableSkeleton } from 'common/components/skeletonPreloaders/TableSkeleton'
+export { TitleBlockSkeleton } from 'common/components/skeletonPreloaders/TitleBlockSkeleton'
+
